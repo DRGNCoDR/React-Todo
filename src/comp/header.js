12f86636d14d94aae5b9fb17ceb859a38ccb54f5
Ray <rayfradella@yahoo.com>
2023-10-03 01:08:47 -0400

    Update header code for readability

--- src/comp/header.js
+++ src/comp/header.js
@@ -7,29 +7,32 @@ const header = {
     padding: '5px',
     borderRadius: "5px",
     fontWeight: "bold"
-  };
+};
 
 function Header({todoCount, completeCount}){
-    const date = new Date().toDateString()
-    const percent = 0
+    const currDate = new Date().toDateString()
+    let percentComplete = 0
+    
     function getCompletePercent(){
-        var percent =
+        var getPercentageValue =
             (todoCount > 0 && completeCount > 0) ?
-                (completeCount/todoCount) *100 :  0
+                (completeCount/todoCount) * 100 : 
+                0        
         
-            percent = (percent > 100) ? 100 : percent
+        percentComplete = (getPercentageValue > 100) ? 100 : getPercentageValue
+        
         return(
-            percent
+            percentComplete
         )
     }
+
     return (
     <div
         style= {header}
     >
         <span>
-            {date}
+            {currDate}
         </span>
-
         <span>
             To Do
         </span>
